51e28908046f49b5b15e73f0b44a62c470b071b2
minhhoang0122 <minhhoang0122@gmail.com>
2026-03-21 03:43:33 +0700

    add data-table

--- frontend/src/app/(admin)/sale/quotes/page.tsx
+++ frontend/src/app/(admin)/sale/quotes/page.tsx
@@ -47,7 +47,7 @@ export default async function QuotesListPage() {
                     </div>
                 ) : (
                 <div className="overflow-x-auto">
-                    <table className="w-full text-left min-w-[1000px] table-fixed">
+                    <table className="data-table w-full text-left min-w-[1000px] table-fixed">
                         <colgroup>
                             <col className="w-[80px]" />
                             <col className="w-[180px]" />
